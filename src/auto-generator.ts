9bc6964bf6a7e44a1ce2faf68aa90012707f9de7
Israel dela Cruz <igdcruz42@gmail.com>
2022-01-30 21:22:28 -0600

    converted enum to declare type

--- src/auto-generator.ts
+++ src/auto-generator.ts
@@ -745,10 +745,10 @@ export class AutoGenerator {
         // override
         isOptional = columnTypeOverride.isOptional;
       } else {
-        isOptional = fieldObj.allowNull && nullableFieldType !== NullableFieldTypes.Null;
+        isOptional = fieldObj.allowNull && nullableFieldType !== "NULL";
       }
       str += `${sp}${name}${isOptional ? '?' : notOptional}: ` +
-        `${columnTypeOverride && columnTypeOverride.type !== undefined ? columnTypeOverride.type : (this.getTypeScriptType(table, field) + (fieldObj.allowNull && nullableFieldType !== NullableFieldTypes.Optional ? " | null" : ""))};\n`;
+        `${columnTypeOverride && columnTypeOverride.type !== undefined ? columnTypeOverride.type : (this.getTypeScriptType(table, field) + (fieldObj.allowNull && nullableFieldType !== "OPTIONAL" ? " | null" : ""))};\n`;
     });
     return str;
   }
